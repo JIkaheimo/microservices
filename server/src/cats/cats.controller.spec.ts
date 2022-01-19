912da79d916ce0f4e7fc56cec3ec1d77b269f79e
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2022-01-19 07:13:14 +0200

    Finish cats controller.

--- server/src/cats/cats.controller.spec.ts
+++ server/src/cats/cats.controller.spec.ts
@@ -1,4 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { randomUUID } from 'crypto';
 import { CatsController } from './cats.controller';
 
 describe('CatsController', () => {
@@ -19,6 +20,30 @@ describe('CatsController', () => {
   describe('findAll', () => {
     it('should return "This action returns all cats"', () => {
       expect(controller.findAll()).toBe('This action returns all cats');
+    });
+  });
+
+  describe('create', () => {
+    it('should return "This action adds a new cat"', () => {
+      expect(controller.create()).toBe('This action adds a new cat');
+    });
+  });
+  describe('findOne', () => {
+    it('should return "This action returns a #{id} cat"', () => {
+      const id = randomUUID();
+      expect(controller.findOne(id)).toBe(`This action returns a #${id} cat`);
+    });
+  });
+
+  describe('getDocs', () => {
+    it('should return undefined when version is omitted', () => {
+      expect(controller.getDocs()).toBeUndefined();
+    });
+    it('should return url when version is 5', () => {
+      expect(controller.getDocs(5)).toHaveProperty(
+        'url',
+        'https://docs.nestjs.com/v5/',
+      );
     });
   });
 });
--- server/src/cats/cats.controller.ts
+++ server/src/cats/cats.controller.ts
@@ -1,9 +1,66 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+  Query,
+  Redirect,
+} from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { UpdateCatDto } from './dto';
+import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(): string {
+  findAll(@Query() query?: { limit: number }) {
+    if (query?.limit)
+      return `This action returns all cats (limit: ${query.limit} items)`;
+
     return 'This action returns all cats';
+  }
+
+  @Post()
+  create(@Body() catData?: CreateCatDto): string {
+    return 'This action adds a new cat';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() catData?: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+
+  // MISC ROUTES
+
+  @Get('async')
+  async findAllAsync(): Promise<string[]> {
+    return ['first', 'second', 'third'];
+  }
+
+  @Get('observable')
+  findAllObservable(): Observable<string[]> {
+    return of(['first', 'second', 'third']);
+  }
+
+  @Get('docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version', ParseIntPipe) version?: number) {
+    if (version == 5) {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
