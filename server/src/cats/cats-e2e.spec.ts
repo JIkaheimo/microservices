fec556cd3569e58fdcc1b2f0233ff030f5d9cd08
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2022-01-19 19:03:12 +0200

    Setup class-validator.

--- server/src/cats/cats-e2e.spec.ts
+++ server/src/cats/cats-e2e.spec.ts
@@ -2,7 +2,7 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { CatsModule } from './cats.module';
 import { CatsService } from './cats.service';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { catMock, catsServiceMock } from './cats.service.mock';
 
 describe('Cats', () => {
@@ -17,14 +17,19 @@ describe('Cats', () => {
       .compile();
 
     app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
-  it(`[GET] /cats`, () => {
+  it('[GET] /cats', () => {
     return request(app.getHttpServer())
       .get('/cats')
       .expect(200)
       .expect([catMock]);
+  });
+
+  it('[POST] /cats', () => {
+    return request(app.getHttpServer()).post('/cats').expect(400);
   });
 
   afterAll(async () => {
