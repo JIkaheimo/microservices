e5931729c4806e5deabf30b5fc870d5e6ea8fa30
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2022-01-24 20:57:04 +0200

    Create BaseInput component.

---
+++ client/components/base/BaseInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="form-group" :class="{ 'has-danger': error }">
+    <label v-if="label" class="form-label" :for="id">{{ label }}</label>
+    <input
+      v-bind="$attrs"
+      :id="id"
+      :value="value"
+      @input="emit('update:value', ($event.target as HTMLInputElement).value)"
+      class="form-control"
+      :class="{ 'is-invalid': error }"
+    />
+    <div class="invalid-feedback" v-if="error">{{ error }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  error?: string;
+  label?: string;
+  value: unknown;
+  id: string;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:value", value: unknown);
+}>();
+</script>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
+</script>
